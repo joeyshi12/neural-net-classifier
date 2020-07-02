198c419182839b3df65692a44ce3bf703da27afc
Joey <joeyshi12@hotmail.com>
2020-07-01 21:54:31 -0700

    changed project structure

--- Main.py
+++ Main.py
@@ -5,49 +5,51 @@ import numpy as np
 from joblib import load
 from tkinter import *
 from tkinter import messagebox
-Tk().wm_withdraw() #to hide the main window
-
+Tk().wm_withdraw()
 pygame.init()
+pygame.display.set_caption('Digit Recognition')
 
-fps = 60
+fps = 160
 fpsClock = pygame.time.Clock()
-length = 560
-screen = pygame.display.set_mode((length, length))
-
-canvas_length = int(length / 10)
-block_length = int(length / canvas_length)
-blocks = []
+surface = pygame.display.set_mode((560, 560))
+canvas = np.zeros((56, 56))
+block_length = 10
+model = load('data/mlp.joblib')
 
 
 def fillSquare(canvas, i, j):
-    if 0 <= i < len(canvas) and 0 <= j < len(canvas[0]):
+    n, d = canvas.shape
+    if 0 <= i < n and 0 <= j < d:
         canvas[i][j] = 1
-    block = pygame.Rect((block_length * j, block_length * i, block_length, block_length))
-    blocks.append(block)
+
+
+def draw_canvas(surface, canvas):
+    n, d = canvas.shape
+    for i in range(n):
+        for j in range(d):
+            if canvas[i][j] == 1:
+                pygame.draw.rect(surface, (0,0,0), (10 * j, 10 * i, block_length, block_length))
 
 
 def compress(canvas):
     retVal = np.zeros((28, 28))
     for i in range(28):
         for j in range(28):
-            retVal[i][j] = (canvas[2*i][2*j] + canvas[2*i+1][2*j] + canvas[2*i][2*j+1] + canvas[2*i+1][2*j+1]) / 4
+            retVal[i][j] = (canvas[2 * i][2 * j] + canvas[2 * i + 1][2 * j] + canvas[2 * i][2 * j + 1] + canvas[2 * i + 1][2 * j + 1]) / 4
     return retVal
 
 
-def main():
-    model = load('model.joblib')
-    canvas = np.zeros((canvas_length, canvas_length))
+if __name__ == '__main__':
     while True:
       for event in pygame.event.get():
         if event.type == QUIT:
           pygame.quit()
           sys.exit()
-        if event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 y_hat = model.predict([compress(canvas).flatten()])[0]
-                messagebox.showinfo('Prediction',y_hat)
-                canvas = np.zeros((canvas_length, canvas_length))
-                blocks.clear()
+                messagebox.showinfo('Prediction', y_hat)
+                canvas.fill(0)
 
       if pygame.mouse.get_pressed()[0]:
           i = int(pygame.mouse.get_pos()[1] / block_length)
@@ -56,13 +58,8 @@ def main():
               for x in [j - 1, j, j + 1]:
                   fillSquare(canvas, y, x)
 
-      screen.fill((255,255,255))
-      for block in blocks:
-          pygame.draw.rect(screen, (0,0,0), block)
+      surface.fill((255,255,255))
+      draw_canvas(surface, canvas)
 
       pygame.display.flip()
       fpsClock.tick(fps)
-
-
-if __name__ == '__main__':
-    main()
--- Model.py
+++ Model.py
@@ -2,28 +2,24 @@ import os
 import pickle
 import gzip
 import argparse
-import time
+import numpy as np
 from joblib import dump
 from sklearn.neural_network import MLPClassifier
-import numpy as np
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-q','--question', required=True)
-
     io_args = parser.parse_args()
     question = io_args.question
 
+    with gzip.open('data/mnist.pkl.gz', 'rb') as f:
+        train_set, valid_set, test_set = pickle.load(f, encoding="latin1")
+    X, y = train_set
+    Xtest, ytest = test_set
+    print("n =", X.shape[0])
+    print("d =", X.shape[1])
+
     if question == 'mlp':
-        with gzip.open('data/mnist.pkl.gz', 'rb') as f:
-            train_set, valid_set, test_set = pickle.load(f, encoding="latin1")
-        X, y = train_set
-        Xtest, ytest = test_set
-
-        print("n =", X.shape[0])
-        print("d =", X.shape[1])
-
         model = MLPClassifier(hidden_layer_sizes=(300,300), verbose=True)
         model.fit(X, y)
 
@@ -37,4 +33,4 @@ if __name__ == '__main__':
         testError = np.mean(yhat != ytest)
         print("Test error     = ", testError)
 
-        dump(model, 'model.joblib')
+        dump(model, 'data/mlp.joblib')
