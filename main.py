2cc211b1c57ca4704965f0f8129f5fde97341dae
Joey <joeyshi12@hotmail.com>
2020-07-30 12:10:51 -0700

    refactored, added jupyter file for training models

---
+++ main.py
@@ -0,0 +1,69 @@
+import pygame
+from pygame.locals import *
+import numpy as np
+from joblib import load
+from tkinter import *
+from tkinter import messagebox
+
+Tk().wm_withdraw()
+pygame.init()
+pygame.display.set_caption('Digit Recognition')
+
+clock = pygame.time.Clock()
+surface = pygame.display.set_mode((560, 560))
+canvas = np.zeros((56, 56))
+block_length = 10
+model = load('trained_model/nn1.joblib')
+
+
+def fill_square(canvas, i, j):
+    n, d = canvas.shape
+    if 0 <= i < n and 0 <= j < d:
+        canvas[i][j] = 1
+
+
+def draw_canvas(surface, canvas):
+    n, d = canvas.shape
+    for i in range(n):
+        for j in range(d):
+            if canvas[i][j] == 1:
+                pygame.draw.rect(surface, (0, 0, 0), (10 * j, 10 * i, block_length, block_length))
+
+
+def compress(canvas):
+    retVal = np.zeros((28, 28))
+    for i in range(28):
+        for j in range(28):
+            retVal[i][j] = (canvas[2 * i][2 * j] + canvas[2 * i + 1][2 * j] + canvas[2 * i][2 * j + 1] +
+                            canvas[2 * i + 1][2 * j + 1]) / 4
+    return retVal
+
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    y_hat = model.predict([compress(canvas).flatten()])[0]
+                    messagebox.showinfo('Prediction', str(y_hat))
+                    canvas.fill(0)
+
+        if pygame.mouse.get_pressed()[0]:
+            i = pygame.mouse.get_pos()[1] // block_length
+            j = pygame.mouse.get_pos()[0] // block_length
+            for y in [i - 1, i, i + 1]:
+                for x in [j - 1, j, j + 1]:
+                    fill_square(canvas, y, x)
+
+        surface.fill((255, 255, 255))
+        draw_canvas(surface, canvas)
+
+        pygame.display.flip()
+        clock.tick(0)
+
+
+if __name__ == '__main__':
+    main()
--- train.py
+++ train.py
@@ -1,46 +1,26 @@
-import os
 import pickle
 import gzip
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-from joblib import dump
-from joblib import load
-from sklearn.neural_network import MLPClassifier
 from model.neural_net import NNClassifier
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-q','--question', required=True)
+    parser.add_argument('-q', '--question', required=True)
     io_args = parser.parse_args()
     question = io_args.question
 
     with gzip.open('data/mnist.pkl.gz', 'rb') as f:
         train_set, valid_set, test_set = pickle.load(f, encoding="latin1")
     X, y = train_set
-    Xtest, ytest = test_set
+    X_test, y_test = test_set
     print("n =", X.shape[0])
     print("d =", X.shape[1])
 
-    if question == 'sklearn':
-        model = MLPClassifier(hidden_layer_sizes=(300,300), verbose=True)
-        model.fit(X, y)
-
-        # Compute training error
-        yhat = model.predict(X)
-        trainError = np.mean(yhat != y)
-        print("Training error = ", trainError)
-
-        # Compute test error
-        yhat = model.predict(Xtest)
-        testError = np.mean(yhat != ytest)
-        print("Test error     = ", testError)
-
-
-    elif question == 'personal':
+    if question == 'train':
         model = NNClassifier(hidden_layer_sizes=[300], lammy=0.01, epochs=20, verbose=True)
         model.fit(X, y)
 
@@ -50,28 +30,22 @@ if __name__ == '__main__':
         print("Training error = ", trainError)
 
         # Compute test error
-        yhat = model.predict(Xtest)
-        testError = np.mean(yhat != ytest)
+        yhat = model.predict(X_test)
+        testError = np.mean(yhat != y_test)
         print("Test error     = ", testError)
 
-
     elif question == 'kmeans':
-        # model = KMeans(n_clusters=10, verbose=1)
-        # model.fit(X, y)
-        # dump(model, 'trained_models/kmeans.joblib')
+        model = KMeans(n_clusters=10, verbose=1)
+        model.fit(X, y)
 
-        model_nn = load('trained_models/mlp.joblib')
-        model_kmeans = load('trained_models/kmeans.joblib')
-
-        fig, ax = plt.subplots(2,5)
+        fig, ax = plt.subplots(2, 5)
         for i in range(2):
             for j in range(5):
-                im = model_kmeans.cluster_centers_[i * 5 + j]
-                ax[i, j].set_title('Digit: %d' %(model_nn.predict([im])[0]))
+                im = model.cluster_centers_[i * 5 + j]
+                ax[i, j].set_title('Digit: %d' % (model.predict([im])[0]))
                 ax[i, j].imshow(1 - im.reshape((28, 28)), cmap='gray')
         fig.tight_layout()
         plt.savefig('figs/kmeans.png')
-
 
     elif question == 'pca':
         model = PCA(2)
@@ -79,13 +53,12 @@ if __name__ == '__main__':
         n = 100
 
         plt.figure()
-        plt.scatter(Z[:n,0], Z[:n,1], c=y[:n])
+        plt.scatter(Z[:n, 0], Z[:n, 1], c=y[:n])
         plt.xlabel("z1")
         plt.ylabel("z2")
         plt.title('PCA')
 
         plt.savefig('figs/pca.png')
-
 
     else:
         print("Unknown question: %s" % question)
